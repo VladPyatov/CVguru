ae86f0d0d0be6c5c4e860fc06f8789c0ecc9f167
VladislavP <vladpyatov@yandex.ru>
2020-06-29 23:07:16 +0300

    task 1.1
    Signed-off-by: VladislavP <vladpyatov@yandex.ru>

--- Module 1: OpenCV/1.1 Load, display, save/load_display_save.py
+++ Module 1: OpenCV/1.1 Load, display, save/load_display_save.py
@@ -1,1 +1,26 @@
 import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+arg_p = argparse.ArgumentParser()
+arg_p.add_argument("-i", "--image", required=True, help="Path to the image")
+arg_p.add_argument("-o", "--output", required=False, help="Output path of the image")
+args = vars(arg_p.parse_args())
+
+# load the image and show some basic information on it
+image = cv2.imread(args["image"])
+print(f"width: {image.shape[1]} pixels")
+print(f"height: {image.shape[0]} pixels")
+print(f"channels: {image.shape[2]}")
+
+# show the image and wait for keypress
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+# save image
+if args["output"]:
+    cv2.imwrite(args["output"], image)
+
+
+
+
