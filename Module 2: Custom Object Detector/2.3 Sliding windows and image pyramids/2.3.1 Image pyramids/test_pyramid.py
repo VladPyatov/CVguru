7d7330c7da205a95a76a7423d1c90f11ecc1f8ad
VladislavP <vladpyatov@yandex.ru>
2020-07-13 02:10:12 +0300

    just add
    Signed-off-by: VladislavP <vladpyatov@yandex.ru>

---
+++ Module 2: Custom Object Detector/2.3 Sliding windows and image pyramids/2.3.1 Image pyramids/test_pyramid.py
@@ -0,0 +1,17 @@
+from helpers import pyramid
+import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to the input image")
+ap.add_argument("-s", "--scale", type=float, default=1.5, help="scale factor size")
+args = vars(ap.parse_args())
+
+# load the input image
+image = cv2.imread(args["image"])
+
+# loop over the layers of the image pyramid and display them
+for (i, layer) in enumerate(pyramid(image, scale=args["scale"])):
+    cv2.imshow(f"Layer {i+1}", layer)
+    cv2.waitKey(0)
